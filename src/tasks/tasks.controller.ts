6e2b50a16823437674eceb541a77bd19ead6ebf1
Francisco Viana <franciscoviana862@gmail.com>
2020-01-19 15:12:31 -0300

    Created: CRUD in memory;

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Put, Delete } from '@nestjs/common';
+import { TaskService } from './shared/task.service';
+import { Task } from './shared/task';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+
+    constructor(
+        private taskSevice: TaskService,
+    ) { }
+
+    @Get()
+    async getAll(): Promise<Task[]> {
+        return this.taskSevice.getAll();
+    }
+
+    @Get(':id')
+    async getById(@Param('id') id: number): Promise<Task> {
+        return this.taskSevice.getById(id);
+    }
+
+    @Post()
+    async create(@Body() task: Task): Promise<Task> {
+        return this.taskSevice.create(task);
+    }
+
+    @Put(':id')
+    async update(@Param('id') id: number, @Body() task: Task): Promise<Task> {
+        task.id = id;
+        return this.taskSevice.update(task);
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id: number) {
+        this.taskSevice.delete(id);
+    }
+}
